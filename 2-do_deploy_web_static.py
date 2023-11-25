756be770dd0e974d3028a860a416f9f2d5951e7f
Perpetual Okorie <giftedwonda@gmail.com>
2023-11-25 11:31:07 +0100

    deploy web static file

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Python Script(Fabric script) (based on the file 1-pack_web_static.py),
+   Distributes an archive to the web servers, using the function do_deploy.
+   Fabric deployment script for web_static
+"""
+from fabric.contrib import files
+from fabric.api import env, put, run, local, runs_once
+import os
+from datetime import datetime
+
+env.hosts = ['52.201.220.59', '54.146.61.93']
+
+
+@runs_once
+def do_pack():
+    """"""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
+    except Exception:
+        output = None
+    return output
+
+
+def do_deploy(archive_path):
+    """Function for deploy"""
+    if not os.path.exists(archive_path):
+        return False
+
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
+
+    try:
+        put(archive_path, '/tmp')
+        run('mkdir -p {}'.format(dest))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('rm -f /tmp/{}.tgz'.format(name))
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+        run('rm -rf {}/web_static'.format(dest))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(dest))
+        return True
+    except Exception:
+        return False
